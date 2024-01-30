b6c9f51b336648068b70dd613aa2cbf57aaa65bf
k-lila <krishnalilapalandi@gmail.com>
2024-01-30 16:58:36 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <h1>Calculadora Aritmética</h1>
+    </div>
+    <div class="calc">
+      <div>
+        <input type="text" id="num1">
+      </div>
+      <div>
+        <div>+</div>
+      </div>
+      <div>
+        <input type="text" id="num2">
+      </div>
+    </div>
+    <div class="result">
+      <span>123</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
